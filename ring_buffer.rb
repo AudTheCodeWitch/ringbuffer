fc5839119410d758d97fb0ebe57d7da69c34f9e5
Audrea Cook <aud@codewitch.dev>
2021-05-25 16:25:07 -0600

    Implement `.new`

--- ring_buffer.rb
+++ ring_buffer.rb
@@ -1,2 +1,10 @@
 class RingBuffer
+  def initialize(capacity:)
+    @arr = Array.new(capacity)
+    @start = 0
+    @end = 0
+
+    raise ArgumentError if capacity < 1
+  end
 end
+
--- spec/ring_buffer_spec.rb
+++ spec/ring_buffer_spec.rb
@@ -1,5 +1,29 @@
 require_relative '../ring_buffer.rb'
 
-RSpec.describe "RingBuffer" do
+RSpec.describe RingBuffer do
+  describe '.new' do
+    subject(:result) { described_class.new(capacity: capacity) }
+
+    context 'with a valid capacity' do
+      let(:capacity) { 4 }
+
+      it { is_expected.to be_a(RingBuffer) }
+
+      it 'does not raise an exception' do
+        expect { result }.to_not raise_error
+      end
+
+      it 'creates an empty array the with the same length as the given capacity' do
+        expect(result.instance_variable_get(:@arr)).to eq([nil, nil, nil, nil])
+      end
+    end
+
+    context 'with an invalid capacity' do
+      let(:capacity) {0}
+      it 'raises an ArgumentError' do
+        expect { result }.to raise_error(ArgumentError)
+      end
+    end
+  end
 
 end
